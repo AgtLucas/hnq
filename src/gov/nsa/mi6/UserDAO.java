44a30455f42dc1de6b20861ed6a8670075e2b7e7
Lucas Churchill <lucas@agtlucas.com>
2014-03-16 10:43:38 -0300

    Update UserDao

--- src/gov/nsa/mi6/UserDAO.java
+++ src/gov/nsa/mi6/UserDAO.java
@@ -6,6 +6,10 @@
 
 package gov.nsa.mi6;
 
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+
 /**
  *
  * @author lucas.silva
@@ -45,6 +49,26 @@ public class UserDAO extends AbstractDAO {
     @Override
     public Object getNewInstance() {
         return new User();
-    }   
+    }
+    
+    protected String getNamedQueryToFindRoles() {
+        return "Role.find.rolesUser";
+    }
+    
+    public List<UserRole> findRoles(User user) throws Exception {
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.getNamedQuery(getNamedQueryToFindRoles());
+            q.setString("user", Integer.toString(user.getId()));
+            List lt = q.list();
+            session.getTransaction().commit();
+            return lt;
+        } catch (HibernateException e) {
+            throw new Exception(e.getCause().getLocalizedMessage());
+        } finally {
+            releaseSession(session);
+        }
+    }
     
 }
